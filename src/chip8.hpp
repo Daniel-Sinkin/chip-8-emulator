e9c46c09ce776f195cf28fca30048975ce7854de
Daniel-Sinkin <danielsinkin97@gmail.com>
2025-05-10 00:08:12 +0200

    started implementing the interface

--- src/chip8.hpp
+++ src/chip8.hpp
@@ -1,11 +1,36 @@
 // danielsinkin97@gmail.com
 #pragma once
 
+#include <array>
+#include <cstdint>
+
 using BYTE = uint8_t;
 using PIXEL = uint8_t; // In principle we could use bool but bit arrays are slower, memory tradeoff worth it
 using WORD = uint16_t;
 
 struct Chip8 {
+    std::array<BYTE, 4 * 1024> mem = {};
     std::array<std::array<PIXEL, 64>, 32> display = {};
+    WORD PC = 0;
+    WORD index_register = 0; // I
+    WORD SP = 0;
+    BYTE delay_timer = 0;
+    BYTE sound_timer = 0;
+    BYTE V0 = 0;
+    BYTE V1 = 0;
+    BYTE V2 = 0;
+    BYTE V3 = 0;
+    BYTE V4 = 0;
+    BYTE V5 = 0;
+    BYTE V6 = 0;
+    BYTE V7 = 0;
+    BYTE V8 = 0;
+    BYTE V9 = 0;
+    BYTE VA = 0;
+    BYTE VB = 0;
+    BYTE VC = 0;
+    BYTE VD = 0;
+    BYTE VE = 0;
+    BYTE VF = 0;
 };
 inline Chip8 chip8;
--- src/render.hpp
+++ src/render.hpp
@@ -1,6 +1,7 @@
 /* danielsinkin97@gmail.com */
 #pragma once
 
+#include "chip8.hpp"
 #include "global.hpp"
 #include "utils.hpp"
 
@@ -9,7 +10,33 @@
 #include "imgui.h"
 #include <glad/glad.h>
 
-inline auto render_gui_debug_panel() -> void {
+namespace Render {
+inline auto display_grid() -> void {
+    constexpr int pixel_size = 10;
+
+    ImGui::Begin("Display");
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+    for (int y = 0; y < 32; ++y) {
+        for (int x = 0; x < 64; ++x) {
+            ImVec4 color = chip8.display[y][x] ? ImVec4(1, 1, 1, 1) : ImVec4(0, 0, 0, 1);
+            ImGui::PushStyleColor(ImGuiCol_Button, color);
+            ImGui::PushStyleColor(ImGuiCol_ButtonHovered, color);
+            ImGui::PushStyleColor(ImGuiCol_ButtonActive, color);
+
+            std::string id = "##px_" + std::to_string(y) + "_" + std::to_string(x);
+            if (ImGui::Button(id.c_str(), ImVec2(pixel_size, pixel_size))) {
+                chip8.display[y][x] ^= 1; // Toggle pixel value
+            }
+
+            ImGui::PopStyleColor(3);
+            if (x < 63) ImGui::SameLine();
+        }
+    }
+    ImGui::PopStyleVar();
+    ImGui::End();
+}
+
+inline auto gui_debug() -> void {
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplSDL2_NewFrame(global.renderer.window);
     ImGui::NewFrame();
@@ -22,11 +49,13 @@ inline auto render_gui_debug_panel() -> void {
         ImGui::Text("Mouse Position: (%.3f, %.3f)", global.input.mouse_pos.x, global.input.mouse_pos.y);
         ImGui::End();
     }
+    display_grid();
     ImGui::Render();
 }
 
-inline auto render_frame() -> void {
+inline auto frame() -> void {
     glViewport(0, 0, (int)global.renderer.imgui_io.DisplaySize.x, (int)global.renderer.imgui_io.DisplaySize.y);
     glClearColor(global.color.background.r, global.color.background.g, global.color.background.b, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+}
 }
